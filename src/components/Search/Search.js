61a5c5e3621a8184e5dca0d1df76d1c4122f69e1
ramanchauhan13 <ramanchauhan13@gmail.com>
2024-08-25 14:04:01 +0530

    carthtml

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Search() {
   return (
-    <div className="h-[calc(100vh-70px)] border-2 border-solid border-black">
-      Search
+    <div className="h-[calc(100vh-70px)] flex justify-center pt-5">
+      <div className='border-2 border-solid border-black h-[50px] w-[500px] flex'>
+        <input className='h-[100%] w-[100%] text-2xl px-2 text-[#FE5005] outline-none' type='text' placeholder='Search Food Here...'></input>
+        <button className='bg-[#FE5005] px-4 font-extrabold border-l-2 border-black'>Search</button>
+      </div>
     </div>
   )
 }
