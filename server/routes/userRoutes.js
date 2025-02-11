f5802eaa3734a3e47a6af273574dbf6cef5676f2
ramanchauhan13 <ramanchauhan13@gmail.com>
2025-02-11 11:56:15 +0530

    kjjhj

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,14 +4,14 @@ const {Signup}=require('../controllers/userController');
 
 const router = express.Router();
 
-const signupSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  mobile: Joi.string().length(10).required(),
-  dob: Joi.date().required(),
-  department: Joi.string().required()
-});
+// const signupSchema = Joi.object({
+//   name: Joi.string().min(3).max(30).required(),
+//   email: Joi.string().email().required(),
+//   password: Joi.string().min(6).required(),
+//   mobile: Joi.string().length(10).required(),
+//   dob: Joi.date().required(),
+//   department: Joi.string().required()
+// });
 
 router.post('/signup', Signup);
   
