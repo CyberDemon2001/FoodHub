d97c9fe2398486de46f8de61608a636a1aa557f4
CyberDemon2001 <vaibhavtyagi254@gmail.com>
2025-03-17 23:08:03 +0530

    little change

--- client/src/components/common/Content.jsx
+++ client/src/components/common/Content.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -9,38 +9,57 @@ import slide2 from "../../assets/slide2.jpg";
 import slide3 from "../../assets/slide3.jpg";
 import slide4 from "../../assets/slide4.jpg";
 import slide5 from "../../assets/slide5.jpg";
-import axios from "axios";
-import { toast } from "react-toastify";
 import { useNavigate, useParams } from "react-router-dom";
-import image from "../../assets/rest.jpg";
-import Menu from "../common/Menu";
 import image1 from "../../assets/restaurant1.jpg";
-import Navbar from "./Navbar";
 
-function Content({ restaurant }) {
+function Content({ restaurant = [] }) {
   const { id } = useParams();
   const navigate = useNavigate();
 
-    // Extracting all unique sections
-    const allSections = restaurant.flatMap((restaurant) =>
-      restaurant.menu ? restaurant.menu.map((section) => section.section) : []
-    );
+  // Extracting all unique sections
+  const allSections = restaurant.flatMap((restaurant) =>
+    restaurant.menu ? restaurant.menu.map((section) => section.section) : []
+  );
+  const uniqueSections = [...new Set(allSections)];
 
-    // Remove duplicates using Set
-    const uniqueSections = [...new Set(allSections)];
-    console.log("Unique Sections:", uniqueSections);
-    
-    const handleViewMenu = (selectedRestaurant) => {
-      if (!id) {
-        navigate(`/home/${selectedRestaurant.restaurantName}`, {
-          state: { restaurant: selectedRestaurant },
-        });
-      } else {
-        navigate(`/user/${id}/${selectedRestaurant.restaurantName}`, {
-          state: { restaurant: selectedRestaurant },
-        });
-      }
-    };
+  const handleViewMenu = (selectedRestaurant) => {
+    const path = id
+      ? `/user/${id}/${selectedRestaurant.restaurantName}`
+      : `/home/${selectedRestaurant.restaurantName}`;
+    navigate(path, { state: { restaurant: selectedRestaurant } });
+  };
+
+  const renderSwiperSlides = (data, isRestaurant = true) => {
+    return data.map((item, index) => (
+      <SwiperSlide key={index}>
+        <div className="border-20 mx-5 my-5 h-[300px] text-center bg-white border-white transition-transform duration-300 ease-in-out transform hover:scale-110 shadow-lg relative overflow-hidden rounded-lg">
+          <img
+            src={image1}
+            className="w-full h-full object-cover rounded-lg"
+            alt={isRestaurant ? item.restaurantName : item}
+          />
+          {isRestaurant && (
+            <>
+              <button
+                className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white w-[50%] rounded-md shadow-lg opacity-90 hover:opacity-100 transition"
+                onClick={() => handleViewMenu(item)}
+              >
+                Open Menu
+              </button>
+              <h1 className="absolute bottom-2 left-0 right-0 text-lg font-bold bg-orange-500 text-black py-3">
+                {item.restaurantName || "Unnamed Restaurant"}
+              </h1>
+            </>
+          )}
+          {!isRestaurant && (
+            <h1 className="absolute bottom-2 left-0 right-0 text-lg font-bold bg-orange-500 text-black py-3">
+              {item}
+            </h1>
+          )}
+        </div>
+      </SwiperSlide>
+    ));
+  };
 
   return (
     <>
@@ -48,7 +67,7 @@ function Content({ restaurant }) {
 
       <div className="border-30 mx-15 my-6 relative border-gray-300 rounded-2xl bg-white h-[65vh]">
         <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
+          modules={[Navigation, Autoplay]}
           navigation
           pagination={{ clickable: true }}
           autoplay={{ delay: 2000 }}
@@ -73,39 +92,22 @@ function Content({ restaurant }) {
         </Swiper>
       </div>
 
-      <div className=" mx-15">
+      <div className="mx-15">
         <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
+          modules={[Navigation, Autoplay]}
           navigation
           pagination={{ clickable: true }}
-          autoplay={{
-            delay: 2000,
-            pauseOnMouseEnter: true,
-          }}
+          autoplay={{ delay: 2000, pauseOnMouseEnter: true }}
           spaceBetween={30}
           slidesPerView={4}
+          breakpoints={{
+            320: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 4 },
+          }}
           loop={true}
         >
-          {restaurant.map((restaurant, index) => (
-            <SwiperSlide key={index}>
-              <div className="border-20 mx-5 my-5  h-[300px] text-center bg-white border-white transition-transform duration-300 ease-in-out transform hover:scale-110 shadow-lg relative overflow-hidden rounded-lg">
-                <img
-                  src={image1}
-                  className="w-full h-full object-cover rounded-lg"
-                  alt={restaurant.restaurantName}
-                />
-                <button
-                  className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white w-[50%] rounded-md shadow-lg opacity-90 hover:opacity-100 transition"
-                  onClick={() => handleViewMenu(restaurant)}
-                >
-                  Open Menu
-                </button>
-                <h1 className="absolute bottom-2 left-0 right-0 text-lg font-bold  bg-orange-500 text-black py-3 ">
-                  {restaurant.restaurantName || "Unnamed Restaurant"}
-                </h1>
-              </div>
-            </SwiperSlide>
-          ))}
+          {renderSwiperSlides(restaurant)}
         </Swiper>
       </div>
 
@@ -114,31 +116,23 @@ function Content({ restaurant }) {
         <span className="text-black text-2xl font-bold">Favorite Food</span>
         <hr className="flex-grow border-2 border-gray-500 mx-20" />
       </div>
-      <div className=" mx-15">
+
+      <div className="mx-15">
         <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
+          modules={[Navigation, Autoplay]}
           navigation
           pagination={{ clickable: true }}
-          autoplay={{
-            delay: 2000,
-            pauseOnMouseEnter: true,
-          }}
+          autoplay={{ delay: 2000, pauseOnMouseEnter: true }}
           spaceBetween={30}
           slidesPerView={4}
+          breakpoints={{
+            320: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 4 },
+          }}
           loop={true}
         >
-          {uniqueSections.map((section, index) => (
-            <SwiperSlide key={index}>
-              <div className="border-20 mx-5 my-5 h-70 text-center bg-white border-white transition-transform duration-300 ease-in-out transform hover:scale-110 shadow-lg">
-                {section}
-                <img
-                  src={image1}
-                  className="w-full h-full object-cover rounded-lg"
-                  alt={restaurant.restaurantName}
-                />
-              </div>
-            </SwiperSlide>
-          ))}
+          {renderSwiperSlides(uniqueSections, false)}
         </Swiper>
       </div>
     </>
