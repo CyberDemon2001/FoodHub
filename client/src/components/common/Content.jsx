948361058f69922e116ea0ac324f41c900df503f
CyberDemon2001 <vaibhavtyagi254@gmail.com>
2025-03-18 10:41:38 +0530

    content

--- client/src/components/common/Content.jsx
+++ client/src/components/common/Content.jsx
@@ -32,31 +32,35 @@ function Content({ restaurant = [] }) {
   const renderSwiperSlides = (data, isRestaurant = true) => {
     return data.map((item, index) => (
       <SwiperSlide key={index}>
-        <div className="border-20 mx-5 my-5 h-[300px] text-center bg-white border-white transition-transform duration-300 ease-in-out transform hover:scale-110 shadow-lg relative overflow-hidden rounded-lg">
-          <img
-            src={image1}
-            className="w-full h-full object-cover rounded-lg"
-            alt={isRestaurant ? item.restaurantName : item}
-          />
-          {isRestaurant && (
-            <>
-              <button
-                className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white w-[50%] rounded-md shadow-lg opacity-90 hover:opacity-100 transition"
-                onClick={() => handleViewMenu(item)}
-              >
-                Open Menu
-              </button>
-              <h1 className="absolute bottom-2 left-0 right-0 text-lg font-bold bg-orange-500 text-black py-3">
-                {item.restaurantName || "Unnamed Restaurant"}
-              </h1>
-            </>
-          )}
-          {!isRestaurant && (
+        {isRestaurant ? (
+          <div className="border-20 mx-5 my-5 h-[300px] text-center bg-white border-white transition-transform duration-300 ease-in-out transform hover:scale-110 shadow-lg relative overflow-hidden rounded-lg">
+            <img
+              src={image1}
+              className="w-full h-full object-cover rounded-lg"
+              alt={item.restaurantName}
+            />
+            <button
+              className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white w-[50%] rounded-md shadow-lg opacity-90 hover:opacity-100 transition"
+              onClick={() => handleViewMenu(item)}
+            >
+              Open Menu
+            </button>
             <h1 className="absolute bottom-2 left-0 right-0 text-lg font-bold bg-orange-500 text-black py-3">
+              {item.restaurantName || "Unnamed Restaurant"}
+            </h1>
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-60 w-60 ">
+            <img
+              src={image1}
+              className="w-[90%] h-[90%] rounded-full object-cover border-10 border-white"
+              alt={item}
+            />
+            <h1 className="h-[10%] text-lg font-bold  text-black text-center">
               {item}
             </h1>
-          )}
-        </div>
+          </div>
+        )}
       </SwiperSlide>
     ));
   };
@@ -81,15 +85,42 @@ function Content({ restaurant = [] }) {
                 alt={`Slide ${index + 1}`}
                 className="w-full h-full object-cover absolute"
               />
-              <h1 className="text-white block bg-white/10 backdrop-blur-xs font-bold text-4xl relative text-center top-1/3">
+              <h1 className="text-white block bg-white/10 font-bold text-4xl relative text-center top-1/3">
                 FoodHub
               </h1>
-              <h1 className="text-white bg-white/10 backdrop-blur-xs text-2xl relative text-center top-1/3">
+              <h1 className="text-white bg-white/10 text-2xl relative text-center top-1/3">
                 Your Favorite Food, Delivered Fast!
               </h1>
             </SwiperSlide>
           ))}
         </Swiper>
+      </div>
+
+      <div className="mx-15">
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          navigation={false}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 2000, pauseOnMouseEnter: true }}
+          spaceBetween={30}
+          slidesPerView={5}
+          breakpoints={{
+            320: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 4 },
+          }}
+          loop={true}
+        >
+          {renderSwiperSlides(uniqueSections, false)}
+        </Swiper>
+      </div>
+
+      <div className="flex items-center justify-center my-6">
+        <hr className="flex-grow border-2 border-gray-500 mx-20" />
+        <span className="text-black text-2xl font-bold">
+          Favorite Restaurants
+        </span>
+        <hr className="flex-grow border-2 border-gray-500 mx-20" />
       </div>
 
       <div className="mx-15">
@@ -109,49 +140,21 @@ function Content({ restaurant = [] }) {
         >
           {restaurant.map((restaurant, index) => (
             <SwiperSlide key={index}>
-              <div onClick={() => handleViewMenu(restaurant)} className="border-20 mx-5 my-5  h-[300px] text-center bg-white border-white transition-transform duration-300 ease-in-out transform hover:scale-110 shadow-lg relative overflow-hidden rounded-lg">
+              <div
+                onClick={() => handleViewMenu(restaurant)}
+                className="border-20 mx-5 my-5 h-[300px] text-center bg-white border-white transition-transform duration-300 ease-in-out transform hover:scale-110 shadow-lg relative overflow-hidden rounded-lg"
+              >
                 <img
                   src={image1}
                   className="w-full h-full object-cover rounded-lg"
                   alt={restaurant.restaurantName}
                 />
-                {/* <button
-                  className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white w-[50%] rounded-md shadow-lg opacity-90 hover:opacity-100 transition"
-                  onClick={() => handleViewMenu(restaurant)}
-                >
-                  Open Menu
-                </button> */}
-                <h1 className="absolute bottom-2 left-0 right-0 text-lg font-bold  bg-orange-500 text-black py-3 ">
+                <h1 className="absolute bottom-2 left-0 right-0 text-lg font-bold bg-orange-500 text-black py-3">
                   {restaurant.restaurantName || "Unnamed Restaurant"}
                 </h1>
               </div>
             </SwiperSlide>
           ))}
-        </Swiper>
-      </div>
-
-      <div className="flex items-center justify-center my-6">
-        <hr className="flex-grow border-2 border-gray-500 mx-20" />
-        <span className="text-black text-2xl font-bold">Favorite Food</span>
-        <hr className="flex-grow border-2 border-gray-500 mx-20" />
-      </div>
-
-      <div className="mx-15">
-        <Swiper
-          modules={[Navigation, Autoplay]}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 2000, pauseOnMouseEnter: true }}
-          spaceBetween={30}
-          slidesPerView={4}
-          breakpoints={{
-            320: { slidesPerView: 1 },
-            768: { slidesPerView: 2 },
-            1024: { slidesPerView: 4 },
-          }}
-          loop={true}
-        >
-          {renderSwiperSlides(uniqueSections, false)}
         </Swiper>
       </div>
     </>
